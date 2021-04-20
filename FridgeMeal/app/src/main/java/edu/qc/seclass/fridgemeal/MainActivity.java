dcf3b7467128adf55b1ec9a8915c62578ef8c949
cchromak <cchromak@gmail.com>
2021-04-20 16:12:47 -0400

    migrated main activity to home fragment,added fragment placeholders for profile and recipes

--- FridgeMeal/app/src/main/java/edu/qc/seclass/fridgemeal/MainActivity.java
+++ FridgeMeal/app/src/main/java/edu/qc/seclass/fridgemeal/MainActivity.java
@@ -1,55 +1,68 @@
 package edu.qc.seclass.fridgemeal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import edu.qc.seclass.fridgemeal.adapters.RecipeAdapter;
+import edu.qc.seclass.fridgemeal.fragments.HomeFragment;
+import edu.qc.seclass.fridgemeal.fragments.ProfileFragment;
+import edu.qc.seclass.fridgemeal.fragments.RecipesFragment;
 import edu.qc.seclass.fridgemeal.models.Recipe;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etIngredient;
-    Button btnAddIngredient;
-    RecyclerView rvHomeRecipes;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
-    List<Recipe> recipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etIngredient = findViewById(R.id.etIngredient);
-        btnAddIngredient = findViewById(R.id.btnAddIngredient);
-        rvHomeRecipes = findViewById(R.id.rvHomeRecipes);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        recipes = new LinkedList<>();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.action_recipes:
+                        fragment = new RecipesFragment();
+                        break;
+                    case R.id.action_logout:
+                        fragment = new HomeFragment();
+                        Toast.makeText(MainActivity.this, "needs to direct to login", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        fragment = new HomeFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
 
-        Recipe chick = new Recipe("Chicken", "34 mins", "You know it, you love it, it should be every meal. It's chicken");
-        Recipe beef = new Recipe("Beef", "100 mins", "You know it, you love it, it should be every meal. It's Beef");
-        Recipe pizza = new Recipe("Pizza", "5 mins", "You know it, you love it, it should be every meal. It's Pizza");
-        recipes.add(chick);
-        recipes.add(beef);
-        recipes.add(pizza);
-
-        // Create Adapter
-        RecipeAdapter recipeAdapter = new RecipeAdapter(this, recipes);
-        // Set Adapter
-        rvHomeRecipes.setAdapter(recipeAdapter);
-        // Set layout manager
-        rvHomeRecipes.setLayoutManager(new LinearLayoutManager(this));
-
-
-
-
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
 
     }
+
 }
