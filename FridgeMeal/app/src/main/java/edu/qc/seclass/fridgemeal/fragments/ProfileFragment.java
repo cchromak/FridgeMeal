34b3b8e52eee2b7ee3a9e677174c55661b6ef365
DarkNightmare42 <errol.me@gmail.com>
2021-04-21 21:01:42 -0400

    Update ProfileFragment.java

--- FridgeMeal/app/src/main/java/edu/qc/seclass/fridgemeal/fragments/ProfileFragment.java
+++ FridgeMeal/app/src/main/java/edu/qc/seclass/fridgemeal/fragments/ProfileFragment.java
@@ -36,7 +36,7 @@ public class ProfileFragment extends Fragment {
     private TextView description;
     private TextView username;
     private Button addBtn;
-    
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
