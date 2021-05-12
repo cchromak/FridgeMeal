56b66cda32b7b392a73c3e14f0cb616b56ed7b68
DarkNightmare42 <errol.me@gmail.com>
2021-05-12 18:34:06 -0400

    replaced missing code

--- FridgeMeal/app/src/main/java/edu/qc/seclass/fridgemeal/models/RecipeDetailActivity.java
+++ FridgeMeal/app/src/main/java/edu/qc/seclass/fridgemeal/models/RecipeDetailActivity.java
@@ -2,6 +2,7 @@ package edu.qc.seclass.fridgemeal.models;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,13 +12,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.parse.ParseUser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import java.util.List;
 
 import java.util.LinkedList;
 
@@ -29,22 +37,24 @@ public class RecipeDetailActivity extends AppCompatActivity {
     TextView tvRecipeNameDetail;
     TextView tvRecipeDescriptionDetail;
     Button btnRecipeDirections;
-    Button faveBox;
-    User user;
     int index = 0;
+    private final String tag = "RecipeDetailActivity";
+    Context context;
+    User user;
+    JSONArray jsonArray;
+    JSONObject jsonObject;
+    JSONObject newObject = new JSONObject();
+    Button faveBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
 
-        user.setUser(ParseUser.getCurrentUser());
-        JSONArray jsonArray = user.getFavorites();
         ivRecipeImageDetail = findViewById(R.id.ivRecipeImageDetail);
         tvRecipeNameDetail = findViewById(R.id.tvRecipeNameDetail);
         tvRecipeDescriptionDetail = findViewById(R.id.tvRecipeDescriptionDetail);
         btnRecipeDirections = findViewById(R.id.btnRecipeDirections);
-        faveBox = findViewById(R.id.faveBox);
         tvRecipeDescriptionDetail.setMovementMethod(new ScrollingMovementMethod());
 
         StringBuilder sb = new StringBuilder();
@@ -53,24 +63,25 @@ public class RecipeDetailActivity extends AppCompatActivity {
         String imagePath = getIntent().getStringExtra("image");
         String directionsPath = getIntent().getStringExtra("directionsPath");
 
-        JSONObject jsonObject = new JSONObject();
+
+        jsonObject = new JSONObject();
         try {
             jsonObject.put("recipeName", getIntent().getStringExtra("recipeName"));
-            jsonObject.put("serving", Integer.parseInt(getIntent().getStringExtra("serving")));
-            jsonObject.put("calories", Integer.parseInt(getIntent().getStringExtra("calories")));
-            jsonObject.put("cookingTime", Integer.parseInt(getIntent().getStringExtra("cookingTime")));
+            jsonObject.put("serving", getIntent().getStringExtra("serving"));
+            jsonObject.put("calories", getIntent().getStringExtra("calories"));
+            jsonObject.put("cookingTime", getIntent().getStringExtra("cookingTime"));
             jsonObject.put("directionsPath", getIntent().getStringExtra("directionsPath"));
             jsonObject.put("image", getIntent().getStringExtra("image"));
             jsonObject.put("ingredientArray", getIntent().getStringExtra("ingredientArray"));
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        
-        if()
+
+        queryUser();
 
         try {
             JSONArray ingredientJSONArray = new JSONArray(ingredientArray);
-            for (int i = 0; i < ingredientJSONArray.length() ; i++) {
+            for (int i = 0; i < ingredientJSONArray.length(); i++) {
                 String currIngredient = ingredientJSONArray.get(i).toString();
                 if (currIngredient.length() != 1) sb.append(currIngredient + "\n\n");
             }
@@ -84,23 +95,80 @@ public class RecipeDetailActivity extends AppCompatActivity {
 
         tvRecipeNameDetail.setText(recipeName);
 
-        for(index = 0; jsonArray.; index++)
-
-        faveBox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(json object does not exist) jsonArray.put(jsonObject);
-                else jsonArray.remove(jsonObject);
-            }
-        });
-
         btnRecipeDirections.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uri = Uri.parse(directionsPath); // missing 'http://' will cause crashed //works fine
+                Uri uri = Uri.parse(directionsPath); // missing 'http://' will cause crashed
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
         });
+    }
+
+
+    protected void queryUser() {
+        Toast.makeText(getBaseContext(), "user query", Toast.LENGTH_SHORT).show();
+        ParseQuery<User> check = ParseQuery.getQuery(User.class);
+        check.include(User.key_user);
+        check.whereEqualTo(User.key_user, ParseUser.getCurrentUser());
+        //query.setLimit(1);
+        check.findInBackground(new FindCallback<User>() {
+            @Override
+            public void done(List<User> users, ParseException e) {
+                if (e != null) {
+                    Log.e(tag, "not null!", e);
+                    return;
+                }
+                jsonArray = users.get(0).getFavorites();
+                Log.i(tag, "List size: " + jsonArray.length());
+                faveBox = findViewById(R.id.faveBox);
+                for(index = 0; index > jsonArray.length(); index++){
+                    try {
+                        newObject = jsonArray.getJSONObject(index);
+                        if (newObject.get("recipeNAme").toString().equals(jsonObject.get("recipeName").toString())) {
+                            faveBox.isPressed();
+                        }
+                    } catch (JSONException jsonException) {
+                        jsonException.printStackTrace();
+                    }
+                }
+                faveBox.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        for (index = 0; index > jsonArray.length(); index++) {
+                            try {
+                                newObject = jsonArray.getJSONObject(index);
+                                if (newObject.get("recipeNAme").toString().equals(jsonObject.get("recipeName").toString())) {
+                                    jsonArray.remove(index);
+                                }
+                                else {
+                                    jsonArray.put(jsonObject);
+                                }
+                            } catch (JSONException jsonException) {
+                                jsonException.printStackTrace();
+                            }
+                        }
+                    }
+                });
+                saveFaves(jsonArray, users);
+            }
+        });
+
+    }
+
+    public void saveFaves(JSONArray jsonArray, List<User> users){
+        User user = new User();
+        user.setUser(users.get(0).getUser());
+        user.setFavorites(jsonArray);
+        user.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null){
+                    Toast.makeText(getBaseContext(), "favorites saved", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
